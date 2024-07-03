aea84e55c4f3ea0d0253e1f0ad7d30035df864d6
mcmah309 <dillonmcmahon79@gmail.com>
2024-07-03 16:04:05 +0000

    feat: Make extensions more general

--- lib/src/vec_extensions.dart
+++ lib/src/vec_extensions.dart
@@ -1,13 +1,13 @@
 import 'vec.dart';
 
-extension VecOnIterableExtension<T extends Object> on Iterable<T> {
+extension VecOnIterableExtension<T> on Iterable<T> {
   @pragma('vm:prefer-inline')
   Vec<T> toVec() {
     return Vec(toList());
   }
 }
 
-extension VecOnIteratorExtension<T extends Object> on Iterator<T> {
+extension VecOnIteratorExtension<T> on Iterator<T> {
   @pragma('vm:prefer-inline')
   Vec<T> collectVec() {
     final list = <T>[];
@@ -18,42 +18,42 @@ extension VecOnIteratorExtension<T extends Object> on Iterator<T> {
   }
 }
 
-extension VecOnListExtension<T extends Object> on List<T> {
+extension VecOnListExtension<T> on List<T> {
   @pragma('vm:prefer-inline')
   Vec<T> asVec() {
     return Vec(this);
   }
 }
 
-extension VecOnListListExtension<T extends Object> on List<List<T>> {
+extension VecOnListListExtension<T> on List<List<T>> {
   @pragma('vm:prefer-inline')
   Vec<T> toFlattenedVec() {
     return Vec(expand((element) => element).toList());
   }
 }
 
-extension VecOnListVecExtension<T extends Object> on List<Vec<T>> {
+extension VecOnListVecExtension<T> on List<Vec<T>> {
   @pragma('vm:prefer-inline')
   Vec<T> toFlattenedVec() {
     return Vec(expand((element) => element).toList());
   }
 }
 
-extension VecOnVecVecExtension<T extends Object> on Vec<Vec<T>> {
+extension VecOnVecVecExtension<T> on Vec<Vec<T>> {
   @pragma('vm:prefer-inline')
   Vec<T> flatten() {
     return Vec(list.expand<T>((element) => element).toList());
   }
 }
 
-extension VecOnVecListExtension<T extends Object> on Vec<List<T>> {
+extension VecOnVecListExtension<T> on Vec<List<T>> {
   @pragma('vm:prefer-inline')
   Vec<T> flatten() {
     return Vec(expand<T>((element) => element).toList());
   }
 }
 
-extension VecSetExtension<T extends Object> on Set<T> {
+extension VecSetExtension<T> on Set<T> {
   @pragma('vm:prefer-inline')
   Vec<T> toVec() {
     return Vec(toList());
